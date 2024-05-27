d5c1652bfad3ed775aeaa80958c63a83130b617b
OkaniYoshiii <dragut31620@gmail.com>
2024-05-27 15:48:04 +0200

    Ajout de la page 'Services'

--- src/controllers/ServicesController.php
+++ src/controllers/ServicesController.php
@@ -6,6 +6,12 @@ class ServicesController implements Controller
 {
     public function getVariables(): array
     {
-        return [];
+        $services = [
+            "Restauration" => "https://picsum.photos/500",
+            "Visite en petit train" => "https://picsum.photos/400",
+            "Visite des habitats" => "https://picsum.photos/600"
+        ];
+
+        return ["services" => $services];
     }
 }
