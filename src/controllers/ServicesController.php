02876581562bed89179adf2d310ca3b329661ff8
OkaniYoshiii <dragut31620@gmail.com>
2024-05-27 17:02:56 +0200

    Modification de la page services en préparation des informations provenant du backend

--- src/controllers/ServicesController.php
+++ src/controllers/ServicesController.php
@@ -7,9 +7,9 @@ class ServicesController implements Controller
     public function getVariables(): array
     {
         $services = [
-            "Restauration" => "https://picsum.photos/500",
-            "Visite en petit train" => "https://picsum.photos/400",
-            "Visite des habitats" => "https://picsum.photos/600"
+            "Restauration" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora ipsa ut, est, tempore necessitatibus odio fuga asperiores dolore blanditiis pariatur expedita consectetur veniam aliquam, maiores perspiciatis? Velit quo nemo deserunt?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi totam delectus, fugiat laborum aut nesciunt temporibus quo, autem minus dicta, ratione at nemo suscipit quas sit. Ipsum commodi alias perferendis.Exercitationem veritatis quasi numquam tempore ad enim fugiat dolorem ratione ut nam dolore quaerat hic impedit inventore omnis minima, saepe quis in commodi facere! Dolorum et eum commodi! Impedit, eius.",
+            "Visite en petit train" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora ipsa ut, est, tempore necessitatibus odio fuga asperiores dolore blanditiis pariatur expedita consectetur veniam aliquam, maiores perspiciatis? Velit quo nemo deserunt?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi totam delectus, fugiat laborum aut nesciunt temporibus quo, autem minus dicta, ratione at nemo suscipit quas sit. Ipsum commodi alias perferendis.Exercitationem veritatis quasi numquam tempore ad enim fugiat dolorem ratione ut nam dolore quaerat hic impedit inventore omnis minima, saepe quis in commodi facere! Dolorum et eum commodi! Impedit, eius.",
+            "Visite des habitats" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora ipsa ut, est, tempore necessitatibus odio fuga asperiores dolore blanditiis pariatur expedita consectetur veniam aliquam, maiores perspiciatis? Velit quo nemo deserunt?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi totam delectus, fugiat laborum aut nesciunt temporibus quo, autem minus dicta, ratione at nemo suscipit quas sit. Ipsum commodi alias perferendis.Exercitationem veritatis quasi numquam tempore ad enim fugiat dolorem ratione ut nam dolore quaerat hic impedit inventore omnis minima, saepe quis in commodi facere! Dolorum et eum commodi! Impedit, eius."
         ];
 
         return ["services" => $services];
