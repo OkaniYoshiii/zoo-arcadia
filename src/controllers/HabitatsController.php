5004f9f194443a80b60db4eeed44269c72001fa7
OkaniYoshiii <dragut31620@gmail.com>
2024-06-08 11:53:22 +0200

    Amélioration section espèces

--- src/controllers/HabitatsController.php
+++ src/controllers/HabitatsController.php
@@ -10,7 +10,7 @@ class HabitatsController implements Controller
             [
                 'name' => 'Savane',
                 'img' => 'bg-savannah-bridge.jpg',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum ornare purus, at egestas massa rhoncus in. Suspendisse pulvinar nibh non laoreet volutpat. Vivamus libero mauris, lacinia ac rhoncus et, efficitur a nisl. Duis turpis nibh, porttitor in felis in, congue dignissim ex. Ut et nisi dapibus, lobortis ligula id, facilisis libero. Nunc arcu arcu, congue ac suscipit sit amet, elementum eget nibh. Nulla tristique cursus ipsum in bibendum. Donec molestie finibus sapien in egestas. Integer semper diam eget velit mattis, vel tincidunt purus semper. Mauris ut aliquam nibh. Duis libero felis, suscipit sed erat at, varius consectetur dui. Proin sollicitudin sed tortor id semper. Phasellus metus leo, aliquam in hendrerit eget, fringilla sit amet metus. Nunc mi risus, facilisis sed ornare a, posuere in mauris. In blandit massa tellus. Nulla maximus erat et eros viverra interdum.',
+                'description' => 'Environement propre aux régions chaudes, la savane regroupe de nombreux animaux comme  zèbres, les gnous, les éléphants, les girafes, les autruches, les gazelles et les buffles. A Arcadia, nous avons fait notre maximum afin de recréer un environnement réprésentant le plus fidèlement possible l\'état naturel de la savane. ',
                 'species' => [
                     [
                         'name' => 'Gnou',
