7d921bd72285e4cf05d8b3b27726e643431e9a94
OkaniYoshiii <dragut31620@gmail.com>
2024-06-14 13:19:30 +0200

    Amélioration validation + stylisation en cas d'erreurs

--- public/assets/js/FormValidation.js
+++ public/assets/js/FormValidation.js
@@ -1,10 +1,13 @@
 export { FormValidation };
 class FormValidation {
     #form;
-    #errors = [];
+    #fields;
+    #errors = {};
     constructor(form) {
         this.#checkArguments(arguments);
         this.#form = form;
+        this.#fields = this.#form.elements;
+        console.log(this.#fields);
         this.#init();
     }
     #init() {
@@ -12,24 +15,26 @@ class FormValidation {
     }
     #validateForm(ev) {
         ev.preventDefault();
-        this.#errors = [];
+        this.#errors = {};
         const emailRegexp = /^[A-Za-z\-\.]+@[A-Za-z\-]+\.[A-Za-z]{2,6}$/;
         const formData = new FormData(this.#form);
         for (const [key, value] of formData) {
             if (key !== 'password' && key !== 'email')
                 window.location.href = '/';
             if (key === 'password') {
-                if (value === '')
-                    this.#errors.push('Le mot de passe ne doit pas être vide.');
+                if (value === '') {
+                    this.#fields;
+                    this.#errors.password = 'Le mot de passe ne doit pas être vide.';
+                }
             }
             if (key === 'email') {
                 if (value === '')
-                    this.#errors.push('L\'email ne doit pas être vide.');
+                    this.#errors.email = 'L\'email ne doit pas être vide.';
                 if (!emailRegexp.test(value.toString()))
-                    this.#errors.push('L\'email doit commencer par une suite de caractères, puis un arobase, une autre suite de caractères et une extension (.com par exemple).');
+                    this.#errors.email = 'L\'email doit commencer par une suite de caractères, puis un arobase, une autre suite de caractères et une extension (.com par exemple).';
             }
         }
-        this.#showErrors();
+        (Object.keys(this.#errors).length > 0) ? this.#showErrors() : this.#form.submit();
     }
     #checkArguments(args) {
         const form = args[0];
@@ -42,9 +47,14 @@ class FormValidation {
         const output = document.getElementById('output');
         if (output === null)
             throw new Error('Aucun element HTML ayant un ID output n\'est présent dans le DOM. Impossible d\'afficher les différentes erreurs de validation.');
+        // RESET
+        Array.from(this.#fields).forEach(field => field.classList.remove('error'));
         output.textContent = '';
-        this.#errors.forEach((error) => {
-            output.innerHTML += error + '<br>';
-        });
+        for (const fieldName in this.#errors) {
+            const fieldInput = this.#fields.namedItem(fieldName);
+            if (fieldInput instanceof HTMLElement)
+                fieldInput.classList.add('error');
+            output.innerHTML += this.#errors[fieldName] + '<br>';
+        }
     }
 }
--- src/typescript/FormValidation.ts
+++ src/typescript/FormValidation.ts
@@ -2,12 +2,15 @@ export { FormValidation };
 
 class FormValidation {
     #form;
-    #errors : string[] = [];
+    #fields : HTMLFormControlsCollection;
+    #errors : { [key: string]: string } = {};
 
     constructor(form : HTMLElement|null) {
         this.#checkArguments(arguments);
 
         this.#form = form as HTMLFormElement;
+        this.#fields = this.#form.elements;
+        console.log(this.#fields);
         this.#init();
     }
 
@@ -17,7 +20,8 @@ class FormValidation {
 
     #validateForm(ev : SubmitEvent) {
         ev.preventDefault();
-        this.#errors = [];
+
+        this.#errors = {};
 
         const emailRegexp = /^[A-Za-z\-\.]+@[A-Za-z\-]+\.[A-Za-z]{2,6}$/;
 
@@ -26,16 +30,19 @@ class FormValidation {
         for (const [key, value] of formData) {
             if(key !== 'password' && key !== 'email') window.location.href = '/';
             if(key === 'password') {
-                if(value === '') this.#errors.push('Le mot de passe ne doit pas être vide.'); 
+                if(value === '') {
+                    this.#fields
+                    this.#errors.password = 'Le mot de passe ne doit pas être vide.';
+                }
             }
                 
             if(key === 'email') {
-                if(value === '') this.#errors.push('L\'email ne doit pas être vide.'); 
-                if(!emailRegexp.test(value.toString())) this.#errors.push('L\'email doit commencer par une suite de caractères, puis un arobase, une autre suite de caractères et une extension (.com par exemple).'); 
+                if(value === '') this.#errors.email = 'L\'email ne doit pas être vide.'; 
+                if(!emailRegexp.test(value.toString())) this.#errors.email ='L\'email doit commencer par une suite de caractères, puis un arobase, une autre suite de caractères et une extension (.com par exemple).'; 
             }
         }
 
-        this.#showErrors();
+        (Object.keys(this.#errors).length > 0) ? this.#showErrors() : this.#form.submit();
     }
 
     #checkArguments(args : IArguments) {
@@ -49,9 +56,15 @@ class FormValidation {
         const output = document.getElementById('output');
 
         if(output === null) throw new Error('Aucun element HTML ayant un ID output n\'est présent dans le DOM. Impossible d\'afficher les différentes erreurs de validation.')
+        
+        // RESET
+        Array.from(this.#fields).forEach(field => field.classList.remove('error'));
         output.textContent = '';
-        this.#errors.forEach((error) => {
-            output.innerHTML += error + '<br>';
-        })
+
+        for (const fieldName in this.#errors) {
+            const fieldInput = this.#fields.namedItem(fieldName);
+            if(fieldInput instanceof HTMLElement) fieldInput.classList.add('error');
+            output.innerHTML += this.#errors[fieldName] + '<br>';
+        }
     }
 }
