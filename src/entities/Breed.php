e560549c14346e1c9781affcdac58cec7bf2ea52
OkaniYoshiii <dragut31620@gmail.com>
2024-06-28 14:09:11 +0200

    Ajout des propriétés/setters/getters de l'entité Breed

--- src/entities/Breed.php
+++ src/entities/Breed.php
@@ -4,8 +4,46 @@ namespace App\Entity;
 
 class Breed 
 {
-    public function __construct()
+    private int $breed_id;
+    private string $name;
+
+    /**
+     * Get the value of breed_id
+     */ 
+    public function getBreedId()
     {
-        
+        return $this->breed_id;
+    }
+
+    /**
+     * Set the value of breed_id
+     *
+     * @return  self
+     */ 
+    public function setBreedId($breed_id)
+    {
+        $this->breed_id = $breed_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
     }
 }
