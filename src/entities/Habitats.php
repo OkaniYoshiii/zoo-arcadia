0cf48412962ff489728879721979cb5da9b019a7
OkaniYoshiii <dragut31620@gmail.com>
2024-06-28 14:30:01 +0200

    Ajout getters/setters entité Habitat

--- src/entities/Habitats.php
+++ src/entities/Habitats.php
@@ -7,4 +7,64 @@ class Habitat
     private int $habitat_id;
     private string $name;
     private string $description;
+
+    /**
+     * Get the value of habitat_id
+     */ 
+    public function getHabitatId()
+    {
+        return $this->habitat_id;
+    }
+
+    /**
+     * Set the value of habitat_id
+     *
+     * @return  self
+     */ 
+    public function setHabitatId($habitat_id)
+    {
+        $this->habitat_id = $habitat_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */ 
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
 }
