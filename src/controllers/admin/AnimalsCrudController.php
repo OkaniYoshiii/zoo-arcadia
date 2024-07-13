47dbff692c690226a8d9b805467c2a01052ff8a2
OkaniYoshiii <dragut31620@gmail.com>
2024-07-13 10:24:26 +0200

    Mise en place controller et template

---
+++ src/controllers/admin/AnimalsCrudController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class AnimalsCrudController 
+{
+    public function getVariables(): array
+    {
+        return [];
+    }
+
+    public function processFormData() : void
+    {
+        
+    }
+}
