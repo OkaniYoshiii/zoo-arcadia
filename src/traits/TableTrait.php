427eb8cfd3f8b360ccc2ad44288d1ae7143cc7c1
OkaniYoshiii <dragut31620@gmail.com>
2024-07-22 10:35:32 +0200

    Modification méthode TRUNCATE de TableTrait - permet de truncate les tables même si des relations sont présentes

--- src/traits/TableTrait.php
+++ src/traits/TableTrait.php
@@ -106,7 +106,7 @@ trait TableTrait
     {
         self::checkConstantsDeclaration();
 
-        Database::$pdo->query('TRUNCATE TABLE ' . self::TABLE_NAME);
+        Database::$pdo->query('FOREIGN_KEY_CHECKS = 0; TRUNCATE TABLE ' . self::TABLE_NAME . '; SET FOREIGN_KEY_CHECKS = 1;');
     }
 
     private static function checkConstantsDeclaration() : void
