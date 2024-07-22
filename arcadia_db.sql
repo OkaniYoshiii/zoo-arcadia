0603e174378409277f2f097669d2b66a18d42fc0
OkaniYoshiii <dragut31620@gmail.com>
2024-07-22 10:33:53 +0200

    Correction erreurs fichier SQL

--- arcadia_db.sql
+++ arcadia_db.sql
@@ -12,45 +12,8 @@ CREATE TABLE users (
     pwd VARCHAR(255) NOT NULL,
     firstname VARCHAR(50),
     lastname VARCHAR(50),
-    role_id INT(11),
+    role_id INT(11) UNSIGNED,
     FOREIGN KEY (role_id) REFERENCES roles(role_id)
-);
-
-CREATE TABLE veterinarian_reports (
-    veterinarian_report_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    date DATE NOT NULL,
-    detail TEXT(1000) NOT NULL,
-    food_quantity DECIMAL(2) NOT NULL,
-    user_id INT(11) NOT NULL,
-    food_type_id INT(11) NOT NULL,
-    food_unit_id INT(11) NOT NULL,
-    animal_id INT(11) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (food_type_id) REFERENCES food_types(food_type_id),
-    FOREIGN KEY (animal_id) REFERENCES animals(animal_id),
-    FOREIGN KEY (food_unit_id) REFERENCES food_units(food_unit_id)
-);
-
-CREATE TABLE food_types (
-    food_type_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(75) NOT NULL
-);
-
-CREATE TABLE animals (
-    animal_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    firstname VARCHAR(25) NOT NULL,
-    state VARCHAR(100),
-    breed_id INT(11),
-    habitat_id INT(11),
-    FOREIGN KEY (breed_id) REFERENCES breeds(breed_id),
-    FOREIGN KEY (habitat_id) REFERENCES habitats(habitat_id)
-);
-
-CREATE TABLE animal_images (
-    animal_image_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    animal_id INT(11),
-    FOREIGN KEY (animal_id) REFERENCES animals(animal_id)
 );
 
 CREATE TABLE breeds (
@@ -65,10 +28,18 @@ CREATE TABLE habitats (
     veterinarian_comments TEXT(500)
 );
 
-CREATE TABLE habitat_images (
-    habitat_image_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    habitat_id INT(11),
+CREATE TABLE food_types (
+    food_type_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(75) NOT NULL
+);
+
+CREATE TABLE animals (
+    animal_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(25) NOT NULL,
+    state VARCHAR(100),
+    breed_id INT(11) UNSIGNED,
+    habitat_id INT(11) UNSIGNED,
+    FOREIGN KEY (breed_id) REFERENCES breeds(breed_id),
     FOREIGN KEY (habitat_id) REFERENCES habitats(habitat_id)
 );
 
@@ -77,13 +48,42 @@ CREATE TABLE food_units (
     name VARCHAR(10) NOT NULL
 );
 
+CREATE TABLE veterinarian_reports (
+    veterinarian_report_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    date DATE NOT NULL,
+    detail TEXT(1000) NOT NULL,
+    food_quantity DECIMAL(2) NOT NULL,
+    user_id INT(11) UNSIGNED NOT NULL,
+    food_type_id INT(11) UNSIGNED NOT NULL,
+    food_unit_id INT(11) UNSIGNED NOT NULL,
+    animal_id INT(11) UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (food_type_id) REFERENCES food_types(food_type_id),
+    FOREIGN KEY (animal_id) REFERENCES animals(animal_id),
+    FOREIGN KEY (food_unit_id) REFERENCES food_units(food_unit_id)
+);
+
+CREATE TABLE animal_images (
+    animal_image_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    animal_id INT(11) UNSIGNED,
+    FOREIGN KEY (animal_id) REFERENCES animals(animal_id)
+);
+
+CREATE TABLE habitat_images (
+    habitat_image_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    habitat_id INT(11) UNSIGNED,
+    FOREIGN KEY (habitat_id) REFERENCES habitats(habitat_id)
+);
+
 CREATE TABLE employee_reports (
     employee_report_id INT(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
     date DATE NOT NULL,
-    food_quantity DECIMAL(2) NOT NULL,
-    food_type_id INT(11) NOT NULL,
-    food_unit_id INT(11) NOT NULL,
-    animal_id INT(11) NOT NULL,
+    food_quantity DECIMAL(2) UNSIGNED NOT NULL,
+    food_type_id INT(11) UNSIGNED NOT NULL,
+    food_unit_id INT(11) UNSIGNED NOT NULL,
+    animal_id INT(11) UNSIGNED NOT NULL,
     FOREIGN KEY (food_type_id) REFERENCES food_types(food_type_id),
     FOREIGN KEY (animal_id) REFERENCES animals(animal_id),
     FOREIGN KEY (food_unit_id) REFERENCES food_units(food_unit_id)
